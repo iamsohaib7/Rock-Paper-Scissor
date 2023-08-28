9377ec43a387da6b86ec9d991942a135caa95370
Sohaib <sohaibt884@gmail.com>
2023-08-28 17:41:10 +0500

    Play Round added

--- script.js
+++ script.js
@@ -5,3 +5,37 @@ function getComputerChoice(){
     let index = Math.floor(Math.random() * gameChoice.length);
     return gameChoice[index];
 }
+function playRound(playerSelection, computerSelection){
+
+    if(playerSelection === "rock" && computerSelection === "paper"){
+        return "You Lose! Paper beats Rock";
+    }
+    else if(playerSelection === "rock" && computerSelection === "scissor"){
+        return "You Won! Rock beats Scissor";
+    }
+    else if(playerSelection === "paper" && computerSelection === "rock"){
+        return "You Won! Paper beats Rock";
+    }
+    else if(playerSelection === "paper" && computerSelection === "scissor"){
+        return "You Lose! Scissor beats Paper";
+    }
+    else if(playerSelection === "scissor" && computerSelection === "paper"){
+        return "You Won! Scissor beats Paper";
+    }
+    else if(playerSelection === "scissor" && computerSelection === "rock"){
+        return "You Won! Rock beats Scissor";
+    }
+    else {
+
+        if(playerSelection === computerSelection){
+            return "Both Selected the same Choice";
+        }
+        else {
+            return "Invalid Selection";
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
